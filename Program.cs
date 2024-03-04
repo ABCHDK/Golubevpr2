66866e29cc1c0dd96fa7c224447b5890f6b2f077
almazikk999 <almazikk999@vk.com>
2024-03-04 10:13:26 +0300

    добавление коммита

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolubevGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 8;
+            Console.WriteLine($"Результат: {a * b}");
+        }
+    }
+}
